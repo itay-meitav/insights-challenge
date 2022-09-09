17165ff7f5a29505f5e75bb97a0b75cc000dec29
itay-meitav <itai9999@gmail.com>
2022-09-09 18:08:57 +0300

    Added loading spinner

--- client/src/components/Index.tsx
+++ client/src/components/Index.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import Accordion from "react-bootstrap/Accordion";
 import config from "../assets/config";
+import Spinner from "react-bootstrap/Spinner";
 
 function Index() {
   const [posts, setPosts] = useState<any[]>([]);
+  const [loader, setLoader] = useState<boolean>(true);
   useEffect(() => {
     fetch(config.apiHost + "posts").then((res) => {
       if (res.ok)
@@ -11,31 +13,45 @@ function Index() {
           setPosts(data);
         });
     });
+    setLoader(false);
   }, []);
   return (
-    <div className="posts" style={{ marginTop: 80, marginBottom: 90 }}>
-      <h5 style={{ marginBottom: 30 }}>Recent Posts</h5>
-      <Accordion style={{ width: 950 }}>
-        {posts.map((element) => {
-          return (
-            <Accordion.Item key={element.id} eventKey={element.id}>
-              <Accordion.Header style={{ position: "relative" }}>
-                {element.title}
-                <div
-                  style={{
-                    position: "absolute",
-                    right: 60,
-                    fontSize: 12,
-                  }}
-                >
-                  {element.info}
-                </div>
-              </Accordion.Header>
-              <Accordion.Body>{element.content}</Accordion.Body>
-            </Accordion.Item>
-          );
-        })}
-      </Accordion>
+    <div className="index">
+      <Spinner
+        animation="border"
+        role="status"
+        style={!loader ? { display: "none" } : { display: "unset" }}
+        className="spinner"
+      >
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+      <div className="posts">
+        <h5 style={{ marginBottom: 30 }}>Recent Posts</h5>
+        <Accordion
+          className="list"
+          style={!loader ? { visibility: "visible" } : { visibility: "hidden" }}
+        >
+          {posts.map((element) => {
+            return (
+              <Accordion.Item key={element.id} eventKey={element.id}>
+                <Accordion.Header style={{ position: "relative" }}>
+                  {element.title}
+                  <div
+                    style={{
+                      position: "absolute",
+                      right: 60,
+                      fontSize: 12,
+                    }}
+                  >
+                    {element.info}
+                  </div>
+                </Accordion.Header>
+                <Accordion.Body>{element.content}</Accordion.Body>
+              </Accordion.Item>
+            );
+          })}
+        </Accordion>
+      </div>
     </div>
   );
 }
